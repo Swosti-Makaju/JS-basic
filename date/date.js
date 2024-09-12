6b323476bd46ce9d02ed7c40472bafb1067a65e5
Swosti-Makaju <swosti.makaju@gmail.com>
2024-09-12 11:59:24 +0545

    js basic

---
+++ date/date.js
@@ -0,0 +1,5 @@
+const today=new Date;
+
+const dateString = today.toDateString();
+
+document.getElementById('date').innerHTML="Today's Date is:"+dateString;
---
+++ variable.js
@@ -0,0 +1,27 @@
+document.getElementById("head").innerHTML="hello";
+// console.log(h1);
+
+
+
+
+
+
+
+
+
+
+
+
+/*let a=1;
+const b=2;
+var c=3;
+if(true){
+      var z=5;
+}
+console.log(z);
+
+const Name="Swosti";
+const id=1;
+const s=15;
+const date=new Date();
+console.log(date,date.getMilliseconds());*/
