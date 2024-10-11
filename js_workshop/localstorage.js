c834f8d0dd46fa40cb5ded598348a45cbe61c6bd
Swosti-Makaju <swosti.makaju@gmail.com>
2024-10-11 20:24:17 +0545

    js

---
+++ js_workshop/localstorage.js
@@ -0,0 +1,8 @@
+// localStorage.setItem("todo", JSON.stringify(obj));
+
+//const data = JSON.parse(localStorage.getItem("todo"));
+//console.log(data);
+
+// localStorage.removeItem("todo");
+
+localStorage.setData("name" ,"ram");
